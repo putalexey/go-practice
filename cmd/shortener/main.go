6896ac24ccac6262974980e48a836f983b2ffa09
Putintcev Alexey <putalexey@gmail.com>
2021-12-09 01:57:55 +0700

    read server configuration via flags

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"github.com/caarlos0/env/v6"
 	"github.com/putalexey/go-practicum/internal/app/shortener"
 	"github.com/putalexey/go-practicum/internal/app/storage"
@@ -25,6 +26,8 @@ func main() {
 		panic(err)
 	}
 
+	parseFlags(&cfg)
+
 	var store storage.Storager
 	if cfg.FileStoragePath != "" {
 		if store, err = storage.NewFileStorage(cfg.FileStoragePath); err != nil {
@@ -34,4 +37,21 @@ func main() {
 
 	router := shortener.NewRouter(cfg.BaseURL, store)
 	log.Fatal(http.ListenAndServe(cfg.Address, router))
+}
+
+func parseFlags(cfg *EnvConfig) {
+	addressFlag := flag.String("a", "", "Адрес запуска HTTP-сервера")
+	baseURLFlag := flag.String("b", "", "Базовый адрес результирующего сокращённого URL")
+	fileStoragePathFlag := flag.String("f", "", "Путь до файла с сокращёнными URL")
+	flag.Parse()
+
+	if *addressFlag != "" {
+		cfg.Address = *addressFlag
+	}
+	if *baseURLFlag != "" {
+		cfg.BaseURL = *baseURLFlag
+	}
+	if *fileStoragePathFlag != "" {
+		cfg.FileStoragePath = *fileStoragePathFlag
+	}
 }
