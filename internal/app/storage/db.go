f3a221526440537a1489115d73e0fe04ca6431b2
Putintcev Alexey <putalexey@gmail.com>
2022-01-07 02:01:11 +0700

    implement db storage

--- internal/app/storage/db.go
+++ internal/app/storage/db.go
@@ -3,6 +3,7 @@ package storage
 import (
 	"context"
 	"database/sql"
+	"fmt"
 	_ "github.com/jackc/pgx/stdlib"
 	"github.com/pressly/goose/v3"
 	"time"
@@ -10,9 +11,10 @@ import (
 
 var _ Storager = &DBStorage{}
 
+var recordsTableName = "shorts"
+var queryTimeout = 5 * time.Second
+
 type DBStorage struct {
-	// temporary extend MemoryStorage to pass tests
-	MemoryStorage
 	db *sql.DB
 }
 
@@ -23,9 +25,6 @@ func NewDBStorage(databaseDSN, migrationsDir string) (*DBStorage, error) {
 	}
 
 	storage := &DBStorage{
-		MemoryStorage: MemoryStorage{
-			records: make(RecordMap),
-		},
 		db: db,
 	}
 
@@ -40,41 +39,74 @@ func NewDBStorage(databaseDSN, migrationsDir string) (*DBStorage, error) {
 	return storage, nil
 }
 
-//func (s *DBStorage) Store(_ context.Context, record Record) error {
-//	//s.db.Exec("INSERT INTO {}")
-//	//s.records[short] = Record{Short: short, Full: full, UserID: userID}
-//	return nil
-//}
+func (s *DBStorage) Store(ctx context.Context, record Record) error {
+	ctx, cancel := context.WithTimeout(ctx, queryTimeout)
+	defer cancel()
 
-//
-//func (s *DBStorage) Load(short string) (string, error) {
-//	//if record, ok := s.records[short]; ok {
-//	//	return record.Full, nil
-//	//}
-//	return "", fmt.Errorf("record \"%s\" not found", short)
-//}
-//
-//func (s *DBStorage) LoadForUser(userID string) ([]Record, error) {
-//	recordList := make([]Record, 0)
-//	//for _, record := range s.records {
-//	//	if record.UserID == userID {
-//	//		recordList = append(recordList, record)
-//	//	}
-//	//}
-//
-//	return recordList, nil
-//}
-//
-//func (s *DBStorage) Delete(short string) error {
-//	//if _, ok := s.records[short]; !ok {
-//	//	return fmt.Errorf("record \"%s\" not found", short)
-//	//}
-//	//delete(s.records, short)
-//	return nil
-//}
+	insertSQL := fmt.Sprintf(`INSERT INTO %s ("short", "original", "user_id") VALUES ($1, $2, $3)`, recordsTableName)
+	_, err := s.db.ExecContext(ctx, insertSQL, record.Short, record.Full, record.UserID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *DBStorage) Load(ctx context.Context, short string) (Record, error) {
+	ctx, cancel := context.WithTimeout(ctx, queryTimeout)
+	defer cancel()
+
+	r := Record{}
+	selectSQL := fmt.Sprintf("SELECT short, original, user_id from %s WHERE short = $1 LIMIT 1", recordsTableName)
+	row := s.db.QueryRowContext(ctx, selectSQL, short)
+	err := row.Scan(&r.Short, &r.Full, &r.UserID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return Record{}, fmt.Errorf("record \"%s\" not found: %w", short, err)
+		}
+		return Record{}, err
+	}
+
+	return r, nil
+}
+
+func (s *DBStorage) LoadForUser(ctx context.Context, userID string) ([]Record, error) {
+	ctx, cancel := context.WithTimeout(ctx, queryTimeout)
+	defer cancel()
+
+	recordList := make([]Record, 0)
+	selectSQL := fmt.Sprintf("SELECT short, original, user_id from %s WHERE user_id = $1", recordsTableName)
+	rows, err := s.db.QueryContext(ctx, selectSQL, userID)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var r Record
+		err := rows.Scan(&r.Short, &r.Full, &r.UserID)
+		if err != nil {
+			return nil, err
+		}
+		recordList = append(recordList, r)
+	}
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return recordList, nil
+}
+
+func (s *DBStorage) Delete(ctx context.Context, short string) error {
+	ctx, cancel := context.WithTimeout(ctx, queryTimeout)
+	defer cancel()
+
+	deleteSQL := fmt.Sprintf("DELETE FROM %s WHERE short = $1", recordsTableName)
+	_, err := s.db.ExecContext(ctx, deleteSQL, short)
+	return err
+}
 
 func (s *DBStorage) Ping(ctx context.Context) error {
-	ctx, cancel := context.WithTimeout(ctx, time.Second)
+	ctx, cancel := context.WithTimeout(ctx, queryTimeout)
 	defer cancel()
 	return s.db.PingContext(ctx)
 }
