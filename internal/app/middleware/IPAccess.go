a8463cc00f0d6b73c6ed6ef15f79dc460d5b98b9
Putintcev Alexey <putalexey@gmail.com>
2022-04-20 02:36:56 +0700

    implement ip restriction

--- internal/app/middleware/IPAccess.go
+++ internal/app/middleware/IPAccess.go
@@ -1,6 +1,8 @@
 package middleware
 
 import (
+	"log"
+	"net"
 	"net/http"
 )
 
@@ -8,7 +10,20 @@ import (
 func IPAccess(trustedSubnet string) func(next http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			var err error
+			trusted := false
 			if len(trustedSubnet) > 0 {
+				trusted, err = isIpTrusted(r.RemoteAddr, trustedSubnet)
+				if err != nil {
+					log.Println(err)
+					w.WriteHeader(http.StatusInternalServerError)
+					w.Write([]byte("Server error"))
+
+					return
+				}
+			}
+
+			if trusted {
 				next.ServeHTTP(w, r)
 			} else {
 				w.WriteHeader(http.StatusForbidden)
@@ -16,4 +31,18 @@ func IPAccess(trustedSubnet string) func(next http.Handler) http.Handler {
 			}
 		})
 	}
+}
+
+func isIpTrusted(addr string, subnet string) (bool, error) {
+	ip, _, err := net.SplitHostPort(addr)
+	if err != nil {
+		return false, err
+	}
+	ip1 := net.ParseIP(ip)
+
+	_, snet, err := net.ParseCIDR(subnet)
+	if err != nil {
+		return false, err
+	}
+	return snet.Contains(ip1), nil
 }
